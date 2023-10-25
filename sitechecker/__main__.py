3fd886300b5b2aeeb92421e66efe7966142636c2
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-10-25 13:21:15 +0200

    fixed script

--- sitechecker/__main__.py
+++ sitechecker/__main__.py
@@ -1,5 +1,5 @@
-from .checker import site_is_online
+from checker import site_is_online
 
-site_is_online("python.org")
+print(site_is_online("python.org"))
 
-site_is_online("non-existing-site.org")
+# site_is_online("non-existing-site.org")
